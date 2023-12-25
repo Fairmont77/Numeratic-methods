a709a313d77f75d7fa92f3fa3b3271bfed36285d
Casper <91886585+Fairmont77@users.noreply.github.com>
2023-12-25 10:30:40 +0200

    Update main.py

--- Lab3/main.py
+++ Lab3/main.py
@@ -4,13 +4,11 @@ import numpy as np
 # Визначення системи нелінійних рівнянь
 def system_of_equations(vars):
     x, y = vars
-    # Система рівнянь, яку потрібно розв'язати
     return [math.sin(x - y) - x*y + 1, x**2 - y**2 - 0.75]
 
 # Визначення матриці Якобі для системи
 def jacobian(vars):
     x, y = vars
-    # Матриця Якобі для системи рівнянь
     return [[math.cos(x - y) - y, -math.cos(x - y) - x], [2*x, -2*y]]
 
 # Функція для обертання матриці
@@ -19,44 +17,43 @@ def invert_matrix(matrix):
     if det == 0:
         raise ValueError("Матриця є сингулярною і не може бути обернена.")
     inv_det = 1 / det
-    # Обернена матриця
     return [[matrix[1][1] * inv_det, -matrix[0][1] * inv_det],
             [-matrix[1][0] * inv_det, matrix[0][0] * inv_det]]
 
 # Модифікований метод Ньютона для розв'язання системи нелінійних рівнянь
 def modified_newton_method(x0, y0, max_iter=10000, tol=1e-10):
     x, y = x0, y0
-    for _ in range(max_iter):
+    iterations = []
+    for iteration in range(max_iter):
         J = jacobian([x, y])
         J_inv = invert_matrix(J)
         F_val = system_of_equations([x, y])
         delta = np.dot(J_inv, F_val)
         x_new = x - delta[0]
         y_new = y - delta[1]
-        # Перевірка збіжності
+        iterations.append((x_new, y_new))
         if abs(x_new - x) < tol and abs(y_new - y) < tol:
-            return x_new, y_new
+            return x_new, y_new, iterations
         x, y = x_new, y_new
     raise ValueError("Розв'язок не було знайдено протягом максимальної кількості ітерацій")
 
 # Визначення ітераційних функцій для методу простої ітерації
 def phi_x(y):
-    # Ітераційна функція для x
     return math.sqrt(0.75 + y**2)
 
 def phi_y(x):
-    # Ітераційна функція для y
     return (1 + math.sin(x)) / x if x != 0 else 0
 
 # Метод простої ітерації для розв'язання системи нелінійних рівнянь
 def simple_iteration_method(x0, y0, max_iter=10000, tol=1e-10):
     x, y = x0, y0
-    for _ in range(max_iter):
+    iterations = []
+    for iteration in range(max_iter):
         x_new = phi_x(y)
         y_new = phi_y(x)
-        # Перевірка збіжності
+        iterations.append((x_new, y_new))
         if abs(x_new - x) < tol and abs(y_new - y) < tol:
-            return x_new, y_new
+            return x_new, y_new, iterations
         x, y = x_new, y_new
     raise ValueError("Розв'язок не було знайдено протягом максимальної кількості ітерацій")
 
@@ -65,14 +62,22 @@ x0, y0 = 1.0, 1.0
 
 # Розв'язання методом модифікованого Ньютона
 try:
-    solution_modified_newton = modified_newton_method(x0, y0)
-    print("Розв'язок модифікованим методом Ньютона:", solution_modified_newton)
+    solution_modified_newton, _, iterations_newton = modified_newton_method(x0, y0)
+    print("Метод модифікованого Ньютона")
+    print("Розв'язок:", solution_modified_newton)
+    print("Кількість ітерацій:", len(iterations_newton))
+    for i, (x, y) in enumerate(iterations_newton, 1):
+        print(f"Ітерація {i}: x = {x}, y = {y}")
 except ValueError as e:
     print(str(e))
 
 # Розв'язання методом простої ітерації
 try:
-    solution_simple_iteration = simple_iteration_method(x0, y0)
-    print("Розв'язок методом простої ітерації:", solution_simple_iteration)
+    solution_simple_iteration, _, iterations_simple = simple_iteration_method(x0, y0)
+    print("\nМетод простої ітерації")
+    print("Розв'язок:", solution_simple_iteration)
+    print("Кількість ітерацій:", len(iterations_simple))
+    for i, (x, y) in enumerate(iterations_simple, 1):
+        print(f"Ітерація {i}: x = {x}, y = {y}")
 except ValueError as e:
     print(str(e))
